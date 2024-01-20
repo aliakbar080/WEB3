2713fe5ae8066a1327603bb388f0640dfafd191b
msi <meibrohimov.@gmail.com>
2024-01-20 14:01:22 +0500

    Initial commit

---
+++ root/urls.py
@@ -0,0 +1,10 @@
+# root url
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("apps.urls"))
+
+]
